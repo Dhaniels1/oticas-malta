90b94de66d80bf0587dab84680aeea53783e7760
Dhaniels1 <danielribeiro4320@gmail.com>
2025-06-01 17:51:21 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -84,7 +84,7 @@ const Home = () => {
             <div className="bg-white rounded-lg shadow-md overflow-hidden">
               <div className="h-64 bg-gray-200 relative">
                 <img 
-                  src={`${process.env.PUBLIC_URL}/9.jpg`}
+                  src={`${process.env.PUBLIC_URL}/images/9.jpg`}
                   alt="Marcas Exclusivas" 
                   className="w-full h-full object-cover"
                 />
